57054256b551f2c4bb2f59a5f7c14c4e14c61632
alwandy <alwandy@live.se>
2016-12-11 06:46:58 -0500

    Made edit work, made delete work and did some reshape on css

--- modules/functions.js
+++ modules/functions.js
@@ -8,6 +8,7 @@
 /*
  * To use the function to retrieve data, please type data.objectname[0].attribute
  */
+function DisplayPrices() {
 $.getJSON('/modules/Prices.json', function(data) {
      $.each(data, function (index, value) {
      for (i = 0; i < value.length; i++) { 
@@ -24,9 +25,9 @@ $.getJSON('/modules/Prices.json', function(data) {
      }
     });
 });
+}
 
-
-
+function EditProductsList() {
 $.getJSON('/modules/Prices.json', function(data) {
      $.each(data, function (index, value) {
      for (i = 0; i < value.length; i++) { 
@@ -36,14 +37,16 @@ $.getJSON('/modules/Prices.json', function(data) {
   '<div class="card-block">' +
     '<h4 class="card-title">'+JSON.stringify(value[i].productname).replace('"', '').replace('"', '')+'</h4>' +
     "<p class='card-text'>"+JSON.stringify(value[i].summary).replace('"', '').replace('"', '')+"</p>" +
-    '<a href="/editproduct/'+JSON.parse(value[i].id)+'" class="btn btn-primary">Edit Product </a>' +
+    '<a href="/viewedit?id='+JSON.parse(value[i].id)+'" class="btn btn-primary">Edit Product </a>' +
  ' </div>' + 
 '</div> </div> '
        );
      }
     });
 });
+}
 
+function Deleteproducts() {
 $.getJSON('/modules/Prices.json', function(data) {
      $.each(data, function (index, value) {
      for (i = 0; i < value.length; i++) { 
@@ -60,8 +63,8 @@ $.getJSON('/modules/Prices.json', function(data) {
      }
     });
 });
-
-
+}
+function ViewTransactions() {
 $.getJSON('/modules/Transactions.json', function(data) {
      $.each(data, function (index, value) {
      for (i = 0; i < value.length; i++) { 
@@ -77,8 +80,119 @@ $.getJSON('/modules/Transactions.json', function(data) {
      }
     });
 });
+}
+function displayValues() {
+$.getJSON('/modules/Prices.json', function(data) {
+     $.each(data, function (index, value) {
+        var i = getAllUrlParams().id;
+       console.log(i);
+     $("#box5").append(
+      '<form id="addForm" action="/viewedit/submit" method="post">' +
+    '</br>' +
+   ' <div class="Add-form">' +
+    '<label for="vendor">Vendor</label>' +
+   ' <input class="form-control" type="text" value="'+ value[i].vendor +'" id="vendor" name="vendor">' +
+  '</div>' +
+  '</br>' +
+    '<div class="Add-form">' +
+    '<label for="productname">Product Name</label>' +
+    '<input class="form-control" type="text" value="'+ value[i].productname +'" id="productname" name="productname">' +
+  '</div>' +
+ ' </br>' +
+    '<div class="Add-form">' +
+    '<label for="price">Product Price</label>' +
+    '<input class="form-control" type="text" value="'+ value[i].price +'" id="price" name="price">' +
+  '</div>' +
+'</br>' +
+  ' <div class="Add-form">' +
+    '<label for="summary">Produt Details</label>' +
+    '<input class="form-control" id="summary" value="'+ value[i].summary +'" rows="3" name="summary"></input>' +
+  '</div>' +
+'</br>' +
+   '<div class="Add-form">' + 
+    '<label for="summary">Produt Image</label>' +
+    '<input class="form-control" id="image" value="'+ value[i].image +'" rows="3" name="image"></input>' +
+  '</div>' +
+'</br>' +
+ '<input type="hidden" id="id" name="id" value="'+ i +'">'+      
+ ' <div class="Add-form">' +
+ '   <label class="form-check-label">' +
+  '    <input type="checkbox" class="form-check-input">' +
+   '   Check when ready to submit!, please check the content before.' +
+   ' </label>' +
+ ' </div>' +
+'</br>' +
+ ' <button type="submit" class="btn btn-primary" name="submit">Submit</button>' +
+'</form>'
+       );
+    });
+});
+}
 
 
+
+
+function getAllUrlParams(url) {
+
+  // get query string from url (optional) or window
+  var queryString = url ? url.split('?')[1] : window.location.search.slice(1);
+
+  // we'll store the parameters here
+  var obj = {};
+
+  // if query string exists
+  if (queryString) {
+
+    // stuff after # is not part of query string, so get rid of it
+    queryString = queryString.split('#')[0];
+
+    // split our query string into its component parts
+    var arr = queryString.split('&');
+
+    for (var i=0; i<arr.length; i++) {
+      // separate the keys and the values
+      var a = arr[i].split('=');
+
+      // in case params look like: list[]=thing1&list[]=thing2
+      var paramNum = undefined;
+      var paramName = a[0].replace(/\[\d*\]/, function(v) {
+        paramNum = v.slice(1,-1);
+        return '';
+      });
+
+      // set parameter value (use 'true' if empty)
+      var paramValue = typeof(a[1])==='undefined' ? true : a[1];
+
+      // (optional) keep case consistent
+      paramName = paramName.toLowerCase();
+      paramValue = paramValue.toLowerCase();
+
+      // if parameter name already exists
+      if (obj[paramName]) {
+        // convert value to array (if still string)
+        if (typeof obj[paramName] === 'string') {
+          obj[paramName] = [obj[paramName]];
+        }
+        // if no array index number specified...
+        if (typeof paramNum === 'undefined') {
+          // put the value on the end of the array
+          obj[paramName].push(paramValue);
+        }
+        // if array index number specified...
+        else {
+          // put the value at that index number
+          obj[paramName][paramNum] = paramValue;
+        }
+      }
+      // if param name doesn't exist yet, set it
+      else {
+        obj[paramName] = paramValue;
+      }
+    }
+  }
+
+  return obj;
+}
 
 var url = window.location.href;
 
--- server.js
+++ server.js
@@ -11,7 +11,9 @@ var bodyParser = require('body-parser');
 //Lets also send the database to that route so we can display all products on first page.
 app.use(express.static(__dirname));
 app.use(bodyParser.json()); // support json encoded bodies
-app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+app.use(bodyParser.urlencoded({
+	extended: true
+})); // support encoded bodies
 
 /*
  *Now when user clicks buy then it will redirect through ID. 
@@ -19,102 +21,112 @@ app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
  */
 app.get('/buyproduct/:id', function(request, response) {
 	var id = request.params.id;
-				fs.readFile(Transactions, 'utf8', function readFileCallback(err, data) {
-				if (err) {
-					 response.redirect('/');
-				} else {
-					obj = JSON.parse(data); //now it an object
-					obj.Transactions.push({
-						id:obj.Transactions.length,
-						date: Date.now(),
-						prodid: Prices.Prices[id].id,
-						total: Prices.Prices[id].price
-					}); //add some data
-					json = JSON.stringify(obj); //convert it back to json
-					fs.writeFile(Transactions, json, 'utf8', response.redirect('/success')); // write it back to disk
-				}
-		})
+	fs.readFile(Transactions, 'utf8', function readFileCallback(err, data) {
+		if (err) {
+			response.redirect('/');
+		} else {
+			obj = JSON.parse(data); //now it an object
+			obj.Transactions.push({
+				id: obj.Transactions.length,
+				date: Date.now(),
+				prodid: Prices.Prices[id].id,
+				total: Prices.Prices[id].price
+			}); //add some data
+			json = JSON.stringify(obj); //convert it back to json
+			fs.writeFile(Transactions, json, 'utf8', response.redirect('/success')); // write it back to disk
+		}
+	})
 });
 
-app.post('/addproduct/submit', function(request, response) { 
+app.post('/addproduct/submit', function(request, response) {
 	var vendor = request.body.vendor;
 	var productname = request.body.productname;
 	var price = request.body.price;
 	var summary = request.body.summary;
 	var image = request.body.image;
-	
-				fs.readFile(Prices, 'utf8', function readFileCallback(err, data) {
-				if (err) {
-					 response.redirect('/');
-				} else {
-					obj = JSON.parse(data); //now it add object
-					obj.Prices.push({
-						id:obj.Prices.length, 
-						vendor:vendor, 
-						productname:productname, 
-						price:price, 
-						summary:summary, 
-						image:image
-					}); //add some data
-					json = JSON.stringify(obj); //convert it back to json
-					fs.writeFile(Prices, json, 'utf8', response.redirect('/')); // write it back to disk
-				}
-		})
+
+	fs.readFile(Prices, 'utf8', function readFileCallback(err, data) {
+		if (err) {
+			response.redirect('/');
+		} else {
+			obj = JSON.parse(data); //now it add object
+			obj.Prices.push({
+				id: obj.Prices.length,
+				vendor: vendor,
+				productname: productname,
+				price: price,
+				summary: summary,
+				image: image
+			}); //add some data
+			json = JSON.stringify(obj); //convert it back to json
+			fs.writeFile(Prices, json, 'utf8', response.redirect('/')); // write it back to disk
+		}
+	})
 });
 
-app.post('/editproduct/submit', function(request, response) { 
+app.get('/viewedit?id=:id', function(request, response) {
+	var id = request.params.id;
+	response.sendFile(__dirname + '/edit2.html');
+});
+
+app.get('/viewedit', function(request, response) {
+	var id = request.params.id;
+	response.sendFile(__dirname + '/edit2.html');
+});
+
+app.post('/viewedit/submit', function(request, response) {
 	var vendor = request.body.vendor;
 	var productname = request.body.productname;
 	var price = request.body.price;
 	var summary = request.body.summary;
 	var image = request.body.image;
-	
-				fs.readFile(Prices, 'utf8', function readFileCallback(err, data) {
-				if (err) {
-					 response.redirect('/');
-				} else {
-					obj = JSON.parse(data); //now it add object
-					obj.Prices.push({
-						id:obj.Prices.length, 
-						vendor:vendor, 
-						productname:productname, 
-						price:price, 
-						summary:summary, 
-						image:image
-					}); //add some data
-					json = JSON.stringify(obj); //convert it back to json
-					fs.writeFile(Prices, json, 'utf8', response.redirect('/')); // write it back to disk
-				}
-		})
+	var id = request.body.id;
+		fs.readFile(Prices, 'utf8', function readFileCallback(err, data) {
+		if (err) {
+			response.redirect('/');
+		} else {
+			obj = JSON.parse(data); //now it add object
+			obj.Prices[id].vendor = vendor; 
+			obj.Prices[id].productname = productname; 
+			obj.Prices[id].price = price; 
+			obj.Prices[id].summary = summary; 
+			obj.Prices[id].image = image; 
+			json = JSON.stringify(obj); //convert it back to json
+			fs.writeFile(Prices, json, 'utf8', response.redirect('/editproduct')); // write it back to disk
+		}
+	})
 });
 
-app.get('/deleteproduct/:id', function(request, response) { 
-		var id = request.params.id;			
-				fs.readFile(Prices, 'utf8', function readFileCallback(err, data) {
-				if (err) {
-					 response.redirect('/');
-				} else {
-					obj = JSON.parse(data); 
-					for(var i = 0; i<obj.Prices.length; i++) {
-						obj.Prices[i].id = obj.Prices[i].id - 1; // Changes the ID for the rest
-					}
-					obj.Prices.splice(id,1); // removes the object 
-					json = JSON.stringify(obj); //convert it back to json
-					fs.writeFileSync(Prices, json, 'utf8', response.redirect('/')); // write it back to disk
-				}
-		})
+
+
+app.get('/deleteproduct/:id', function(request, response) {
+	var id = request.params.id;
+	fs.readFile(Prices, 'utf8', function readFileCallback(err, data) {
+		if (err) {
+			response.redirect('/');
+		} else {
+			obj = JSON.parse(data);
+			for (var i = 0; i < obj.Prices.length; i++) {
+				obj.Prices[i].id = obj.Prices[i].id - 1; // Changes the ID for the rest
+			}
+			obj.Prices.splice(id, 1); // removes the object 
+			json = JSON.stringify(obj); //convert it back to json
+			fs.writeFileSync(Prices, json, 'utf8', response.redirect('/')); // write it back to disk
+		}
+	})
 });
+
 
 app.get('/listTransaction/:id', function(request, response) {
 	var id = request.params.id;
-				fs.readFile(Transactions, 'utf8', function readFileCallback(err, data) {
-				if (err) {
-					 response.redirect('/');
-				} else {
-					obj = JSON.parse(data); //now it an object
-					var Transactions = Transactions[id];
-				}
-		})
+	fs.readFile(Transactions, 'utf8', function readFileCallback(err, data) {
+		if (err) {
+			response.redirect('/');
+		} else {
+			obj = JSON.parse(data); //now it an object
+			var Transactions = Transactions[id];
+		}
+	})
 });
 
 
@@ -131,9 +143,7 @@ app.get('/editproduct', function(request, response) {
 	response.sendFile(__dirname + '/edit.html');
 });
 
-app.get('/deleteproduct', function(request, response) {
-	response.sendFile(__dirname + '/delete.html');
-});
+
 
 app.get('/transaction', function(request, response) {
 	response.sendFile(__dirname + '/listTransactions.html');
